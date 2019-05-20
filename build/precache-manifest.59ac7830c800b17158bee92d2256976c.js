b88191b6fa8c7a379f41bdfc014780962eb3d803
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2019-05-20 21:28:54 +0500

    by Numan Latif Muhammad

---
+++ build/precache-manifest.59ac7830c800b17158bee92d2256976c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a805b308fb104b0d829034c2f3cdb999",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f0cf25efe8f93a64d85",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ff95acf393f7664bcb5d",
+    "url": "/static/css/main.59eaef63.chunk.css"
+  },
+  {
+    "revision": "9f0cf25efe8f93a64d85",
+    "url": "/static/js/2.d021f8f2.chunk.js"
+  },
+  {
+    "revision": "ff95acf393f7664bcb5d",
+    "url": "/static/js/main.d3597890.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
